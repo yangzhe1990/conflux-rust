f78b31f64b28016cf37ccacb125cdc26bb4866c6
Ming Wu <ming@alt-chain.io>
2018-07-30 19:32:20 +0800

    nit

--- core/sync/src/lib.rs
+++ core/sync/src/lib.rs
@@ -1,10 +1,31 @@
+extern crate network;
+extern crate ethcore_io as io;
+use std::sync::{Arc};
+#[macro_use] 
+extern crate log;
+
 mod api;
 
 pub use api::*;
 
-use network::{NetworkProtocolHandler, NetworkContext, 
-              PeerId, ProtocolId};
+use network::{NetworkService, NetworkProtocolHandler, NetworkContext, NetworkConfiguration,
+              PeerId, ProtocolId, Error};
 use io::{TimerToken};
+
+/// Sync configuration
+#[derive(Debug, Clone, Copy)]
+pub struct SyncConfig {
+    /// Main "cfx" subprotocol name.
+    pub subprotocol_name: [u8; 3],
+}
+
+/// ConfluxSync initialization parameters.
+pub struct Params {
+    /// Configuration.
+    pub config: SyncConfig,
+    /// Network layer configuration.
+    pub network_config: NetworkConfiguration,
+}
 
 /// Conflux network sync engine
 pub struct ConfluxSync {
@@ -17,7 +38,18 @@ pub struct ConfluxSync {
 }
 
 impl ConfluxSync {
-
+    /// Create and register protocol with the network service
+    pub fn new(params: Params) -> Result<Arc<ConfluxSync>, Error> {
+        let service = NetworkService::new(&params.network_config)?;
+        let sync = Arc::new(ConfluxSync {
+            network: service,
+            sync_handler: Arc::new(SyncProtocolHandler {
+                
+            }),
+            subprotocol_name: params.config.subprotocol_name,
+        });
+        Ok(sync)
+    }
 }
 
 struct SyncProtocolHandler {
@@ -27,16 +59,18 @@ impl NetworkProtocolHandler for SyncProtocolHandler {
     fn initialize(&self, io: &NetworkContext) {
     }
 
-	fn read(&self, io: &NetworkContext, peer: &PeerId, packet_id: u8, data: &[u8]) {
+    fn on_message(
+        &self, io: &NetworkContext, peer: PeerId, msg_id: u8, data: Vec<u8>,
+    ) {
     }
 
-	fn connected(&self, io: &NetworkContext, peer: &PeerId) {
+    fn on_peer_connected(&self, io: &NetworkContext, peer: PeerId) {
     }
 
-	fn disconnected(&self, io: &NetworkContext, peer: &PeerId) {
+	fn on_peer_disconnected(&self, io: &NetworkContext, peer: PeerId) {
     }
 
-	fn timeout(&self, io: &NetworkContext, timer: TimerToken) {
+	fn on_timeout(&self, io: &NetworkContext, timer: TimerToken) {
     }
 }
 
@@ -47,6 +81,12 @@ impl ChainNotify for ConfluxSync {
     }
 
     fn start(&self) {
+        match self.network.start() {
+            Err(err) => {
+                warn!("Error starting network");
+            },
+            _ => {},
+        }
     }
 
     fn stop(&self) {
