bdce26e71a6e07246dc7014cd0ae272c98fa170d
Ming Wu <ming@alt-chain.io>
2018-08-04 16:24:47 +0800

    implement acount store interface

--- account-store/src/api.rs
+++ account-store/src/api.rs
@@ -1,26 +1,26 @@
 use ethereum_types::{Address};
 use super::AccountEntry;
 
-pub trait AccountStoreInterface {
-    /// update state of account ac, insert ac if not exists
-    /// return true if inserts, otherwise false
-    fn update_entry(ac: &Address, ent: &AccountEntry) -> bool;
+pub trait AccountStoreInterface: Sync + Send {
+    /// update state of account ac, insert if ac not exists
+    /// return old entry if ac exists, otherwise None
+    fn update_entry(&self, ac: &Address, ent: AccountEntry) -> Option<AccountEntry>;
     
     /// update value of account ac, if ac not exists, do nothing and return false
-    fn update_value(ac: &Address, val: f64) -> bool;
+    fn update_value(&self, ac: &Address, val: f64) -> bool;
 
     /// update nonce of account ac, if ac not exists, do nothing and return false
-    fn set_nonce(ac: &Address, nonce: u64) -> bool;
+    fn set_nonce(&self, ac: &Address, nonce: u64) -> bool;
 
     /// get value of account
-    fn get_value(ac: &Address) -> Option<f64>;
+    fn get_value(&self, ac: &Address) -> Option<f64>;
 
     /// get nonce of account
-    fn get_nonce(ac: &Address) -> Option<u64>;
+    fn get_nonce(&self, ac: &Address) -> Option<u64>;
 
-    /// increment nonce of account by 1, if ac not exists, do nothing and return false
-    fn inc_nonce(ac: &Address) -> bool;    
+    /// increment nonce of account by 1
+    fn inc_nonce(&self, ac: &Address) -> Option<u64>;    
 
     /// check whether account exists
-    fn exist(ac: &Address) -> bool;
+    fn exist(&self, ac: &Address) -> bool;
 }
--- account-store/src/lib.rs
+++ account-store/src/lib.rs
@@ -6,11 +6,13 @@ mod api;
 
 use network::Error;
 use std::collections::HashMap;
+use std::collections::hash_map::Entry::{Occupied, Vacant};
 use ethereum_types::{Address};
 use api::AccountStoreInterface;
 use std::sync::{Arc};
 use parking_lot::RwLock;
 
+/// TODO: use per account rwlock to improve concurrency
 pub struct AccountEntry {
     val: f64,
     nonce: u64,
@@ -31,31 +33,61 @@ impl AccountStore {
 }
 
 impl AccountStoreInterface for AccountStore {
-    fn update_entry(ac: &Address, ent: &AccountEntry) -> bool {
-        true
+    fn update_entry(&self, ac: &Address, ent: AccountEntry) -> Option<AccountEntry> {
+        let mut ac_table = self.table.write();
+        ac_table.insert(ac.clone(), ent)
     }
 
-    fn update_value(ac: &Address, val: f64) -> bool {
-        true
+    fn update_value(&self, ac: &Address, val: f64) -> bool {
+        let mut ac_table = self.table.write();
+        if let Some(entry) = ac_table.get_mut(ac) {
+            entry.val = val;
+            true
+        } else {
+            false
+        }
     }
 
-    fn set_nonce(ac: &Address, nonce: u64) -> bool {
-        true
+    fn set_nonce(&self, ac: &Address, nonce: u64) -> bool {
+        let mut ac_table = self.table.write();
+        if let Some(entry) = ac_table.get_mut(ac) {
+            entry.nonce = nonce;
+            true
+        } else {
+            false
+        }
     }
 
-    fn get_value(ac: &Address) -> Option<f64> {
-        None
+    fn get_value(&self, ac: &Address) -> Option<f64> {
+        let ac_table = self.table.read();
+        if let Some(entry) = ac_table.get(ac) {
+            Some(entry.val)
+        } else {
+            None
+        }
     }
 
-    fn get_nonce(ac: &Address) -> Option<u64> {
-        None
+    fn get_nonce(&self, ac: &Address) -> Option<u64> {
+        let ac_table = self.table.read();
+        if let Some(entry) = ac_table.get(ac) {
+            Some(entry.nonce)
+        } else {
+            None
+        }        
     }
 
-    fn inc_nonce(ac: &Address) -> bool {
-        true
+    fn inc_nonce(&self, ac: &Address) -> Option<u64> {
+        let mut ac_table = self.table.write();
+        if let Some(entry) = ac_table.get_mut(ac) {
+            entry.nonce += 1;
+            Some(entry.nonce)            
+        } else {
+            None
+        }
     }
 
-    fn exist(ac: &Address) -> bool {
-        true
+    fn exist(&self, ac: &Address) -> bool {
+        let ac_table = self.table.read();
+        ac_table.contains_key(ac)
     }
 }
