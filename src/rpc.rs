9b7a75c28b8a300cf0f8c349623a6bdc9cb46cba
Fan Long <fanl@csail.mit.edu>
2018-08-05 20:21:02 +0800

    rpc stubs

--- src/rpc.rs
+++ src/rpc.rs
@@ -1,4 +1,4 @@
-use jsonrpc_core::{IoHandler, Value};
+use jsonrpc_core::{IoHandler, Value, Params, Error};
 use parity_reactor::TokioRemote;
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use tcp::ServerBuilder as TcpServerBuilder;
@@ -29,6 +29,22 @@ impl TcpConfiguration {
     }
 }
 
+macro_rules! parse_u64 {
+    ($a:expr, $k:expr, $m:expr) => {
+        {
+            let x = $a.get($k);
+            if x == None {
+                return Err(Error::invalid_params(format!("{} {} {}", $k, "not specified in", $m)));
+            }
+            let y = x.unwrap().as_u64();
+            if y == None {
+                return Err(Error::invalid_params(format!("{} {} {}", $k, "must be integer in", $m)));
+            }
+            y.unwrap()
+        }
+    };
+}
+
 pub fn new_tcp(
     conf: TcpConfiguration, dependencies: &Dependencies,
 ) -> Result<Option<TcpServer>, String> {
@@ -38,6 +54,39 @@ pub fn new_tcp(
 
     let mut handler = IoHandler::new();
     handler.add_method("say_hello", |_| Ok(Value::String("Hello, world!".into())));
+    // make_active makes the client to enter the active synchronization mode.
+    handler.add_method("make_active", |_| {
+        unimplemented!();
+        Ok(Value::String("Not implemented!".into()))
+    });
+    // make_passive makes the client to enter the passive synchronization mode. Passive mode is
+    // mainly used for debugging.
+    handler.add_method("make_passive", |_| {
+        unimplemented!();
+        Ok(Value::String("Not implemented!".into()))
+    });
+    // generate_blocks makes the client to generate one or more blocks. 
+    handler.add_method("generate_blocks", |_param: Params| {
+        match _param {
+            Params::Map(m) => {
+                let num = parse_u64!(m, "num", "generate_blocks");
+                Ok(Value::String(format!("{}{}", "generate_blocks get ", num)))
+            }
+            _ => Err(Error::invalid_params("Invalid format for \"generate_blocks\""))
+        }
+    });
+    handler.add_method("add_peer", |_param: Params| {
+        unimplemented!();
+        Ok(Value::String("Not implemented!".into()))
+    });
+    handler.add_method("drop_peer", |_param: Params| {
+        unimplemented!();
+        Ok(Value::String("Not implemented!".into()))
+    });
+    handler.add_method("sync_status", |_param: Params| {
+        unimplemented!();
+        Ok(Value::String("Not implemented!".into()))
+    });
 
     let remote = dependencies.remote.clone();
 
