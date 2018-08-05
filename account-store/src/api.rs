f349b7e88513da42c8768a93efff3bfaae4fcf8c
Ming Wu <ming@alt-chain.io>
2018-08-05 11:22:38 +0800

    implement multi-version store for account

--- account-store/src/api.rs
+++ account-store/src/api.rs
@@ -1,18 +1,19 @@
 use ethereum_types::{Address};
 use super::AccountEntry;
+use std::collections::HashMap;
 
 pub trait AccountStoreInterface: Sync + Send {
     /// update state of account ac, insert if ac not exists
     /// return old entry if ac exists, otherwise None
     fn update_entry(&self, ac: &Address, ent: AccountEntry) -> Option<AccountEntry>;
     
-    /// update value of account ac, if ac not exists, do nothing and return false
-    fn update_value(&self, ac: &Address, val: f64) -> bool;
+    /// update value of account ac at version ver, if ac not exists, do nothing and return false
+    fn update_value(&self, ac: &Address, val: f64, ver: u64) -> bool;
 
     /// update nonce of account ac, if ac not exists, do nothing and return false
     fn set_nonce(&self, ac: &Address, nonce: u64) -> bool;
 
-    /// get value of account
+    /// get latest value of account
     fn get_value(&self, ac: &Address) -> Option<f64>;
 
     /// get nonce of account
@@ -23,4 +24,10 @@ pub trait AccountStoreInterface: Sync + Send {
 
     /// check whether account exists
     fn exist(&self, ac: &Address) -> bool;
+
+    /// rollback to snapshot at version ver, generate snapshot in-place
+    fn rollback_to(&self, ver: u64);
+
+    /// get snapshot at version ver
+    fn get_snapshot(&self, ver: u64) -> HashMap<Address, AccountEntry>;
 }
--- account-store/src/lib.rs
+++ account-store/src/lib.rs
@@ -6,15 +6,20 @@ mod api;
 
 use network::Error;
 use std::collections::HashMap;
+use std::collections::BTreeMap;
 use ethereum_types::{Address};
 use api::AccountStoreInterface;
 use std::sync::{Arc};
 use parking_lot::RwLock;
 
-/// TODO: use per account rwlock to improve concurrency
-pub struct AccountEntry {
+pub struct AccountState {
     val: f64,
+}
+
+pub struct AccountEntry {
+    latest_version: u64,
     nonce: u64,
+    versioned_states: BTreeMap<u64, AccountState>,
 }
 
 pub struct AccountStore {
@@ -37,10 +42,13 @@ impl AccountStoreInterface for AccountStore {
         ac_table.insert(ac.clone(), ent)
     }
 
-    fn update_value(&self, ac: &Address, val: f64) -> bool {
+    fn update_value(&self, ac: &Address, val: f64, ver: u64) -> bool {
         let mut ac_table = self.table.write();
         if let Some(entry) = ac_table.get_mut(ac) {
-            entry.val = val;
+            if entry.latest_version < ver {
+                entry.latest_version = ver;
+            }
+            entry.versioned_states.entry(ver).or_insert(AccountState { val:0.0 }).val = val;
             true
         } else {
             false
@@ -60,7 +68,12 @@ impl AccountStoreInterface for AccountStore {
     fn get_value(&self, ac: &Address) -> Option<f64> {
         let ac_table = self.table.read();
         if let Some(entry) = ac_table.get(ac) {
-            Some(entry.val)
+            let ver = entry.latest_version;
+            if let Some(state) = entry.versioned_states.get(&ver) {
+                Some(state.val)
+            } else {
+                None
+            }
         } else {
             None
         }
@@ -88,5 +101,12 @@ impl AccountStoreInterface for AccountStore {
     fn exist(&self, ac: &Address) -> bool {
         let ac_table = self.table.read();
         ac_table.contains_key(ac)
+    }
+
+    fn rollback_to(&self, ver: u64) {
+    }
+
+    fn get_snapshot(&self, ver: u64) -> HashMap<Address, AccountEntry> {
+        HashMap::new()
     }
 }
