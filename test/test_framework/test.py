0b12053138ae70a1b35dc78509af00ec65b425d9
Fan Long <fanl@csail.mit.edu>
2018-08-19 23:53:49 +0800

    update test for sync. The sync is not working yet...

--- test/test_framework/test.py
+++ test/test_framework/test.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from test_node import TestNode
+from time import sleep
 
 x = TestNode(0, "/tmp", "127.0.0.1", 31001, 32001, 33001, "../../target/debug/conflux")
 y = TestNode(1, "/tmp", "127.0.0.1", 31002, 32002, 33002, "../../target/debug/conflux")
@@ -15,5 +16,10 @@ print(x.getblockcount())
 x.generate(3)
 print(x.getblockcount())
 print(x.getbestblockhash())
+print(y.getblockcount())
+print(y.getbestblockhash())
+x.add_peer("127.0.0.1:31002")
+sleep(5)
+print("After sleep!")
 print(y.getblockcount())
 print(y.getbestblockhash())
