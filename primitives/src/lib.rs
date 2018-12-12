f06f6bf9514290887b707f7669c3f31f695c9478
Dong Zhou <dongz@conflux-chain.org>
2018-12-12 16:39:54 +0800

    add LogEntry
    Summary: add LogEntry
    
    Test Plan: [x] build
    
    Reviewers: ming
    
    Reviewed By: ming
    
    Differential Revision: https://phabricator.conflux-chain.org/D131

--- primitives/src/lib.rs
+++ primitives/src/lib.rs
@@ -3,14 +3,18 @@ extern crate ethereum_types;
 extern crate ethkey;
 extern crate keccak_hash as hash;
 extern crate rlp;
+#[macro_use]
+extern crate rlp_derive;
+extern crate heapsize;
 extern crate unexpected;
 
-pub type EpochNumber = u64;
+pub type CardinalNumber = u64;
 
 pub mod account;
 pub mod block;
 pub mod block_header;
 pub mod epoch;
+pub mod log_entry;
 pub mod transaction;
 
 pub use account::Account;
@@ -18,5 +22,5 @@ pub use block::Block;
 pub use block_header::{BlockHeader, BlockHeaderBuilder};
 pub use epoch::EpochId;
 pub use transaction::{
-    SignedTransaction, Transaction, TransactionWithSignature, Action,
+    Action, SignedTransaction, Transaction, TransactionWithSignature,
 };
