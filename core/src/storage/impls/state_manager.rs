7ebfc230edc8808edcf1d08905090128e4262cae
Zhe Yang <yz@conflux-chain.org>
2019-04-07 21:24:13 +0200

    Adjustments and more logging for conflux eth replay test.

--- core/src/storage/impls/state_manager.rs
+++ core/src/storage/impls/state_manager.rs
@@ -135,7 +135,7 @@ impl StateManager {
         let addr = kp.address();
         let account = Account::new_empty_with_balance(
             &addr,
-            &U256::from_dec_str("5000000000000000000000000000")
+            &U256::from_dec_str("5000000000000000000000000000000000")
                 .expect("Not overflow"),
             &0.into(),
         );
