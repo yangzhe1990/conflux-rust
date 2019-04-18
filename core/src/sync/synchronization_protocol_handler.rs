cf6e24018f23b31851497064d91b5f2e08cb4ecb
Zhe Yang <yz@conflux-chain.org>
2019-04-18 20:47:28 +0200

    Speed up recover_public method when receiving block, by looking up in tx pool first.

--- core/src/sync/synchronization_protocol_handler.rs
+++ core/src/sync/synchronization_protocol_handler.rs
@@ -1015,6 +1015,7 @@ impl SynchronizationProtocolHandler {
             }
             Self::recover_public(
                 &mut block,
+                self.get_transaction_pool(),
                 &mut *self
                     .get_transaction_pool()
                     .transaction_pubkey_cache
@@ -1124,6 +1125,7 @@ impl SynchronizationProtocolHandler {
         let mut block = new_block.block;
         Self::recover_public(
             &mut block,
+            self.get_transaction_pool(),
             &mut *self.get_transaction_pool().transaction_pubkey_cache.write(),
             &mut *self.graph.cache_man.lock(),
             &*self.get_transaction_pool().worker_pool.lock(),
@@ -2056,7 +2058,7 @@ impl SynchronizationProtocolHandler {
     }
 
     pub fn recover_public(
-        block: &mut Block,
+        block: &mut Block, tx_pool: SharedTransactionPool,
         tx_cache: &mut HashMap<H256, Arc<SignedTransaction>>,
         cache_man: &mut CacheManager<CacheId>, worker_pool: &ThreadPool,
     ) -> Result<(), DecoderError>
@@ -2067,10 +2069,13 @@ impl SynchronizationProtocolHandler {
         for (idx, transaction) in block.transactions.iter().enumerate() {
             match tx_cache.get(&transaction.hash()) {
                 Some(tx) => recovered_transactions.push(tx.clone()),
-                None => {
-                    uncached_trans.push((idx, transaction.clone()));
-                    recovered_transactions.push(transaction.clone());
-                }
+                None => match tx_pool.get_transaction(&transaction.hash()) {
+                    Some(tx) => recovered_transactions.push(tx.clone()),
+                    None => {
+                        uncached_trans.push((idx, transaction.clone()));
+                        recovered_transactions.push(transaction.clone());
+                    }
+                },
             }
         }
         if uncached_trans.len() < WORKER_COMPUTATION_PARALLELISM * 8 {
